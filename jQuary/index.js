a57079a7a12caac7855833a813aa289bf8ec995f
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-09-24 19:34:56 +0900

    prototype is going on

--- jQuary/index.js
+++ jQuary/index.js
@@ -1,7 +1,34 @@
-let person1 = {};
-console.log(person1);
+// let person1 = {};
+// console.log(person1);
 
-let person2 = {
-    name: "bappy",
-};
-console.log(person2);
+// let person2 = {
+//     name: "bappy",
+// };
+
+// function Person3(name) {
+//     this.name = name;
+// }
+// const friend = new Person3("hamim");
+// console.log(friend);
+
+// console.log(person2);
+// console.log(person2.hasOwnProperty("name"));
+// console.log(person1.hasOwnProperty("prototype"));
+
+function Person(first, last, a) {
+    this.firstName = first;
+    this.lastName = last;
+    this.a = a;
+
+    // this.fullName = function() {
+    //     console.log(this.firstName + " " + this.lastName);
+    // }
+}
+Person.prototype.fullName = function() {
+    console.log(this.firstName + " " + this.lastName);
+}
+
+let person1 = new Person("Siful", "Bappy", 24);
+let person2 = new Person("Hamim", "Buiha", 25);
+
+console.log(person1);

70c9de4a41a4cf1cd8524e7e082ae8a937c7b7b1
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-09-25 17:40:04 +0900

    constractor function is been called

--- jQuary/class_and_object.js
+++ jQuary/class_and_object.js
@@ -1,19 +1,32 @@
-class Person{
-    constructor(first, last) {
-        this.first = first;
-        this.last = last; 
-        // console.log("The person is been created"); 
-    }
+// class Person{
+//     constructor(first, last) {
+//         this.first = first;
+//         this.last = last; 
+//         // console.log("The person is been created"); 
+//     }
 
-    sayHigh() {
-        console.log("The person has been");
-    }
+//     sayHigh() {
+//         console.log("The person has been");
+//     }
 
-    static hello() {
-        console.log("The hello function is been create");
-    }
-}
+//     static hello() {
+//         console.log("The hello function is been create");
+//     }
+// }
 
-let person1 = new Person("Siful", "Bappy");
-Person.hello();
-console.log(person1); 
+// let person1 = new Person("Siful", "Bappy");
+// Person.hello();
+// console.log(person1); 
+
+class emp{
+    constructor() {
+        console.log("construct is been created");
+    }
+};
+
+class manager extends emp{
+
+};
+
+const manager1 = new manager();
+console.log(manager1 );
